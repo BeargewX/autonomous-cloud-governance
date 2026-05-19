7eed3c46b1856483726fd6681f9bd427ac466d23
SPARE-IT-OHAO\admin <66010292@gmail.com>
2026-05-19 15:16:39 +0700

    Fix gitignore and add lambda files

--- terraform/modules/infrastructure/lambda.tf
+++ terraform/modules/infrastructure/lambda.tf
@@ -1,14 +1,14 @@
 # Zip Lambda functions
 data "archive_file" "self_healing" {
   type        = "zip"
-  source_file = "${path.module}/../../../lambda/self_healing.py"
-  output_path = "${path.module}/../../../lambda/self_healing.zip"
+  source_file = "${path.root}/../../lambda/self_healing.py"
+  output_path = "${path.root}/../../lambda/self_healing.zip"
 }
 
 data "archive_file" "finops" {
   type        = "zip"
-  source_file = "${path.module}/../../../lambda/finops.py"
-  output_path = "${path.module}/../../../lambda/finops.zip"
+  source_file = "${path.root}/../../lambda/finops.py"
+  output_path = "${path.root}/../../lambda/finops.zip"
 }
 
 # IAM Role สำหรับ Lambda
